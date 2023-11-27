8b4244903fc2ce8875d6565619fa97ff70dee389
ShirazKachlon <shiraz.kachlon@autobrains.ai>
2023-11-27 17:46:05 +0200

    importing const

--- src/eval/pre_process_data.py
+++ src/eval/pre_process_data.py
@@ -1,3 +1,4 @@
+from src.eval.consts import evaluation_gt_columns
 from src.eval.utils import load_tsv_to_df, save_df_to_tsv
 
 
@@ -22,7 +23,6 @@ class DataPreparations():
     def convert_al_det_to_gt(self, input_file_path):
         output_file_path = input_file_path.replace('.tsv', '_as_gt.tsv')
         df = load_tsv_to_df(input_file_path)
-        columns_to_keep = ['x_center', 'y_center', 'width', 'height', 'label', 'score', 'is_occluded', 'is_truncated', 'd3_separation', 'l_label', 'r_label', 'is_rider_on_2_wheels']
-        gt_df = df[columns_to_keep]
+        gt_df = df[evaluation_gt_columns]
         save_df_to_tsv(gt_df, output_file_path)
         return output_file_path
