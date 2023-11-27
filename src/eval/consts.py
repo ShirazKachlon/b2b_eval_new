9971834986a834d4cf05635a415fb95ece1826eb
ShirazKachlon <shiraz.kachlon@autobrains.ai>
2023-11-27 18:08:22 +0200

    consts file

---
+++ src/eval/consts.py
@@ -0,0 +1,3 @@
+evaluation_gt_columns = ['name', 'x_center', 'y_center', 'width', 'height', 'label', 'd3_separation', 'r_label',
+                         'l_label', 'score']
+evaluation_detection_columns = ['name', 'x_center', 'y_center', 'width', 'height', 'label', 'd3_separation', 'score']
